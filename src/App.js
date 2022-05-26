05ee745405d84acd0d7381b06578b48a07825e41
Deva-Mari <devamari@gmx.net>
2022-05-26 13:54:11 +1200

    added desktop styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import styles from "./App.module.css";
 import Balance from "./components/Balance";
 import Spending from "./components/Spending";
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <div className={styles.content}>
       <Balance/>
       <Spending/>
       <Footer/>
